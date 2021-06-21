7dd1b1826fcea6bc89d6736f737f4477f2d5cbff
SimoFerre98 <Simoneferrero98@gmail.com>
2021-06-21 19:18:17 +0200

    Ultimato funzionamento del Toggle-Switch
    Ora la connessione col bluetooth non blocca l'intero fragment ma funziona solo grazie ad un toggle switch che notifica con un toast il suo funzonamento.
    é stata aggiunta la visibiliti del fab solo con certi fragment

--- CoolUp/app/src/main/java/com/Simo_Elia/CoolUp/MainActivity.kt
+++ CoolUp/app/src/main/java/com/Simo_Elia/CoolUp/MainActivity.kt
@@ -4,12 +4,17 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.transition.Visibility.MODE_OUT
 import android.view.*
 import android.widget.*
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.InputStream
 import java.io.OutputStream
@@ -20,36 +25,14 @@ class MainActivity : AppCompatActivity()  {
     private lateinit var myToolbar: androidx.appcompat.widget.Toolbar
 
     var Toggle : Switch ?=null
-
+    private var fab : FloatingActionButton ?= null
 
     // Settings when launching the application
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //*------------------------------------------------
-
-
-
-
-        /*
-        // Stampa lettere dell'alfabeto
-
-        var outputStream: OutputStream =btSocket.outputStream
-        outputStream.write(40)
-
-        var inputStream :InputStream = btSocket.inputStream;
-        // Per pulire il buffer precedente mettendo lo skip passando per parametro la dimensione dello inputStream
-        inputStream.skip(inputStream.available().toLong());
-
-        for( i in 1..26){
-            var b : Byte=  inputStream.read().toByte()
-            println(b.toChar())
-        }*/
-
-
-        //*------------------------------------------------
-
+        fab = findViewById(R.id.fab)
         // Toolbar association
         myToolbar = findViewById(R.id.myToolbar)
         myToolbar.title=""
@@ -78,6 +61,7 @@ class MainActivity : AppCompatActivity()  {
     // Listener for BottomNavigation
     private val navListener: BottomNavigationView.OnNavigationItemSelectedListener =
         object : BottomNavigationView.OnNavigationItemSelectedListener {
+            @RequiresApi(Build.VERSION_CODES.Q)
             override fun onNavigationItemSelected(item: MenuItem): Boolean {
 
                 val fridge= fridge()
@@ -88,27 +72,38 @@ class MainActivity : AppCompatActivity()  {
                 when (item.itemId) {
                     // Fridge Option
                     R.id.MyFridge ->
-                    supportFragmentManager.beginTransaction().apply {
+                    {
+                        fab!!.setVisibility(View.VISIBLE)
+                        supportFragmentManager.beginTransaction().apply {
                         replace(R.id.flFragment, fridge)
                         commit()
+                        }
                     }
                     // ShopList Option
                     R.id.MyShoppingList ->
-                    supportFragmentManager.beginTransaction().apply {
-                        replace(R.id.flFragment, shoplist)
-                        commit()
+                    {
+                        fab!!.setVisibility(View.VISIBLE)
+                        supportFragmentManager.beginTransaction().apply {
+                            replace(R.id.flFragment, shoplist)
+                            commit()
+                        }
                     }
                     // Search Option
                     R.id.Search ->
-                    supportFragmentManager.beginTransaction().apply {
-                        replace(R.id.flFragment, search)
-                        commit()
+                    {
+                            fab!!.setVisibility(View.INVISIBLE)
+                        supportFragmentManager.beginTransaction().apply {
+                            replace(R.id.flFragment, search)
+                            commit()
+                        }
                     }
                     // Settings Option
                     R.id.Settings ->
+                    {
+                        fab!!.setVisibility(View.INVISIBLE)
                     supportFragmentManager.beginTransaction().apply {
                         replace(R.id.flFragment, settings)
-                        commit()
+                        commit()}
                     }
                 }
                 return true
--- CoolUp/app/src/main/java/com/Simo_Elia/CoolUp/settings.kt
+++ CoolUp/app/src/main/java/com/Simo_Elia/CoolUp/settings.kt
@@ -3,13 +3,15 @@ package com.Simo_Elia.CoolUp
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.FragmentActivity
+import kotlinx.android.synthetic.*
 import java.io.InputStream
 import java.util.*
 
@@ -28,45 +30,67 @@ private const val ARG_PARAM2 = "param2"
 val mUUID : UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
 
 
-class settings : Fragment(R.layout.fragment_settings)  {
+class settings : Fragment()  {
 
-       var value: Any=BarCode()
+    var Toggle:Switch ?= null;
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View?
+    {
+        val view:View = inflater.inflate(R.layout.fragment_settings,container,false)
+
+        Toggle = view.findViewById<Switch>(R.id.Toggle)
+        Toggle!!.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener{
+            buttonView, isChecked ->
+            if(isChecked)
+            {
+                Toast.makeText(context,"Bluetooth Acceso",Toast.LENGTH_LONG).show()
+                var value: Any=BarCode()
+            }else
+            {
+                Toast.makeText(context,"Bluetooth Spento",Toast.LENGTH_LONG).show()
+            }
+        })
 
 
-}
-
-fun BarCode(){
-    // BlueTooth Connections
-    var btAdapter : BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-    println("************************* TUTTI I DISPOSITIVI :"+btAdapter.bondedDevices)
-    var hc05 : BluetoothDevice = btAdapter.getRemoteDevice("00:20:12:08:C2:EB")
-    println("************************* NOME DISPOSITIVO :"+hc05.name)
-
-    // Try to connect with 3 attempts
-    var btSocket: BluetoothSocket
-    var counter = 0;
-    do{
-        btSocket = hc05.createRfcommSocketToServiceRecord(mUUID);
-        println("************************* NOME SOCKET :"+btSocket);
-        btSocket.connect()
-        println("************************* SOCKET CONNESSO? :"+btSocket.isConnected)
-        counter++
-    } while (!btSocket.isConnected && counter < 3)
-
-    // Ricevere la Stringa
-    var Barcode: String=""
-    var inputStream : InputStream = btSocket.inputStream;
-    inputStream.skip(inputStream.available().toLong());
-    do{
-        var SingleChar: Char = inputStream.read().toChar()
-        Barcode+=SingleChar
+        return  view
     }
-    while(inputStream.available()>0)
-    println("************************* BARCODE :"+Barcode)
+
+    fun BarCode(){
+        // BlueTooth Connections
+        var btAdapter : BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        println("************************* TUTTI I DISPOSITIVI :"+btAdapter.bondedDevices)
+        var hc05 : BluetoothDevice = btAdapter.getRemoteDevice("00:20:12:08:C2:EB")
+        println("************************* NOME DISPOSITIVO :"+hc05.name)
+
+        // Try to connect with 3 attempts
+        var btSocket: BluetoothSocket
+        var counter = 0;
+        do{
+            btSocket = hc05.createRfcommSocketToServiceRecord(mUUID);
+            println("************************* NOME SOCKET :"+btSocket);
+            btSocket.connect()
+            println("************************* SOCKET CONNESSO? :"+btSocket.isConnected)
+            counter++
+        } while (!btSocket.isConnected && counter < 3)
+
+        // Ricevere la Stringa
+        var Barcode: String=""
+        var inputStream : InputStream = btSocket.inputStream;
+        inputStream.skip(inputStream.available().toLong());
+        do{
+            var SingleChar: Char = inputStream.read().toChar()
+            Barcode+=SingleChar
+        }
+        while(inputStream.available()>0)
+        println("************************* BARCODE :"+Barcode)
 
 
-    btSocket.close()
-    println("************************* SOCKET CONNESSO? :"+btSocket.isConnected)
+        btSocket.close()
+        println("************************* SOCKET CONNESSO? :"+btSocket.isConnected)
+
+    }
 
 }
+
+
 
